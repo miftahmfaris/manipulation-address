74e04e0a07ab0eee24f150d656cfb38b16c94df9
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-04-03 16:51:30 +0700

    Update index.js and index.html

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 var arrayAddress = []
 
 function addAddress() {
-  var name = document.getElementById("fullName").value;
-
-  arrayAddress.push(name)
+  var objectAddress={};
+  objectAddress.fullName = document.getElementById("fullName").value;
+  objectAddress.address = document.getElementById("address").value;
+  objectAddress.phoneNumber = document.getElementById("phoneNumber").value;
+  arrayAddress.push(objectAddress);
   return arrayAddress;
 }
 
 function showAddress() {
-  return arrayAddress
+}
+
+document.getElementById("addButton").addEventListener("click",function() {
+  localStorage.setItem("arrayAddress", JSON.stringify(arrayAddress))
+  for (var i = 0; i < arrayAddress.length; i++) {
+    var list = document.createElement("li");
+    var listAddress = document.createTextNode(arrayAddress[i].fullName);
+  }
+
+  list.appendChild(listAddress);
+  document.body.appendChild(list);
+  localStorage.getItem("arrayAddress",JSON.parse(arrayAddress))
+  console.log(arrayAddress);
+})
+
+function clearStorage() {
+  localStorage.clear();
 }
